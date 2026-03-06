7fc4a32b54fab5a55d3b6a22e08c176db3ab0213
CINAR <cinemrepro@gmail.com>
2026-03-06 14:20:05 +0100

    corrections des erreurs pr pytest

--- tests/test_objet.py
+++ tests/test_objet.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.objet import objet, Consommable, PotionSoin, Bombe, Equipement
+from src.objet import Objet, Consommable, PotionSoin, Bombe, Equipement
 from src.Personnage import Personnage
 
 
@@ -18,13 +18,11 @@ class FauxPersonnage(Personnage):
 # ----------------------
 def test_objet_init_attributs():
     class ObjetConcret(Objet):
-        """Petite sous-classe concrète pour tester l'init de Objet."""
         pass
 
     o = ObjetConcret("Épée rouillée", "Une vieille épée toute abîmée")
     assert o.nom == "Épée rouillée"
     assert o.description == "Une vieille épée toute abîmée"
-
 
 # ----------------------
 # Tests Consommable / PotionSoin
